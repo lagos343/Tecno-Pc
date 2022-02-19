eae1cd68d866055fa5cc336c9b4c335008af00ff
Alex Tabora <alextabora844@gmail.com>
2022-02-19 11:42:58 -0600

    Clase contacto

--- Formularios/frm_contactos.cs
+++ Formularios/frm_contactos.cs
@@ -7,11 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Tecno_Pc.Formularios
 {
     public partial class frm_contactos : Form
     {
+        bool actualizar = false;
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        //Clases. con = new Clases.CLContactos();
+        //Clases.Cl_SqlMaestra sql = new Clases.Cl_SqlMaestra();
         public frm_contactos()
         {
             InitializeComponent();
